da79cfd24074b875a01455f795dc64cfc19d0501
Boldeusz <mnpower@poczta.onet.pl>
2019-12-30 17:22:20 +0100

    update

--- src/com/example/test/Example.java
+++ src/com/example/test/Example.java
@@ -1,6 +1,7 @@
 package com.example.test;
 
 import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Example {
@@ -10,10 +11,18 @@ public class Example {
         System.out.println(result);
     }
     private static int divide(){
-        int x = getInt();
-        int y = getInt();
-        System.out.println("x is " + x + ", y is " + y);
-        return x / y;
+        int x, y;
+        try{
+            x = getInt();
+            y = getInt();
+            System.out.println("x is " + x + ", y is " + y);
+            return x / y;
+
+        }catch (NoSuchElementException e){
+            throw new ArithmeticException("no suitable input");
+        }catch (ArithmeticException e) {
+            throw new ArithmeticException(" attempt to divide by zero");
+        }
     }
     private static int getInt(){
         Scanner s = new Scanner(System.in);
